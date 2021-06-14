40835736a46c795308ba6cea38b08692532f2c7e
datn02 <ducanh02.work@gmail.com>
2021-06-14 20:10:16 +0800

    Publicize Code

---
+++ global.h
@@ -0,0 +1,20 @@
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define PITCH_RATE 	3.0f     // lower this to get faster rates
+#define ROLL_RATE 	3.0f     // lower this to get faster rates
+#define YAW_RATE		3.0f
+
+//bool angle;					 // auto level status
+//uint8_t start; 			 // ARM status
+#define REFRESH_RATE 		250  // in Hz
+#define LOOPTIME 				4000 // in microseconds
+
+
+
+//#define DEBUG_UART
+
+#endif
